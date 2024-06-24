281639793c1c927b844eec78e04d9ca9e2cd9f2f
서연 <155455327+seoyeon1123@users.noreply.github.com>
2024-06-24 18:43:23 +0900

    feat: header

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Routes/Home';
+import TV from './Routes/TV';
+import Search from './Routes/Search';
+import Header from './Components/Header';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/tv" element={<TV />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Routes/TV.tsx
@@ -0,0 +1,9 @@
+const TV = () => {
+  return (
+    <>
+      <h1>TV</h1>
+    </>
+  );
+};
+
+export default TV;
